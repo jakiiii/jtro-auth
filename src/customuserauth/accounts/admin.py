d0c74fae71c09efb6b059815cb495d7a6be1ea29
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-05 19:59:26 +0600

    initial commit

---
+++ src/customuserauth/accounts/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+
+User = get_user_model()
+
+
+# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    list_display = ('email', 'admin')
+    list_filter = ('admin', 'staff', 'active')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('admin', 'staff', 'active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+         ),
+    )
+
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ('email',)
+    filter_horizontal = ()
+
+    # class Meta:
+    #     model = User
+
+
+admin.site.register(User, UserAdmin)
---
+++ src/customuserauth/accounts/forms.py
@@ -0,0 +1,56 @@
+"""
+This tutorial is codingforentrepreneurs by Justin
+https://www.codingforentrepreneurs.com/blog/how-to-create-a-custom-django-user-model/
+"""
+
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+User = get_user_model()
+
+
+# Create your form here.
+class UserAdminCreationForm(forms.ModelForm):
+    password1 = forms.CharField(max_length=32, label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(max_length=32, label='Password Confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email'
+        )
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Password have to match")
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserAdminCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdminChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'active',
+            'admin'
+        )
+
+    def clean_password(self):
+        return self.initial["password"]
