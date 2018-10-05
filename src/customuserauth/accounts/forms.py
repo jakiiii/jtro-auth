7145c7b4ed2b2e7f2bd46deaa135d2d781393bb8
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-05 20:29:50 +0600

    create custom UserLoginForm and UserRegistrationForm

--- src/customuserauth/accounts/forms.py
+++ src/customuserauth/accounts/forms.py
@@ -54,3 +54,38 @@ class UserAdminChangeForm(forms.ModelForm):
 
     def clean_password(self):
         return self.initial["password"]
+
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(max_length=32)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(max_length=32, label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(max_length=32, label='Password Confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email'
+        )
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Password have to match')
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserRegistrationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        user.is_active = False
+
+        if commit:
+            user.save()
+        return user
